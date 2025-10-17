521f39ae4eca97c56720bc3edbab87996ecbe2f7
Waqar-Ahmad4762 <wa700038@gmail.com>
2025-10-17 16:32:17 +0500

    handled real time text updates using onChange on text inputs

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+     <Main />
+    </>
+  )
+}
+
+export default App
